6f9fa1dd06060839b7010b4cb6710ef22c0c4f8e
leo <psilva.leo@gmail.com>
2026-01-20 16:07:57 -0300

    fix: explicit matching on unit type in shutdown signal

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,8 @@ async fn shutdown_signal() {
     let terminate = std::future::pending::<()>();
 
     tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
+        () = ctrl_c => {},
+        () = terminate => {},
     }
 
     eprintln!("signal received, starting graceful shutdown");
